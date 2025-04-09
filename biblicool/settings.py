abdf85cd2a990b3541686dadfa7037332c2785a3
victoire-dnzt <victoire.denizot@student.isae-supaero.fr>
2025-04-09 22:01:53 +0200

    images

--- biblicool/settings.py
+++ biblicool/settings.py
@@ -130,7 +130,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
